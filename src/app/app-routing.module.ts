f70e88bec5f84d3b14456bc7a235e5495b440b4b
SAURABH SAGAR <mail.saurabhsagar@gmail.com>
2021-03-23 15:37:36 +0530

    Added Lazy Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  { path: 'users', loadChildren: () => import(`./modules/users/users.module`).then(m => m.UsersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
